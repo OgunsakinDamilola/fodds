6af8a5cf5a6c897781bb58d809c654cfb5609719
enochval <osarenrenenoch@gmail.com>
2018-02-21 16:11:57 +0100

    add seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+         $this->call(BanksTableSeeder::class);
+         $this->call(CollateralsTableSeeder::class);
+         $this->call(DesignationsTableSeeder::class);
+         $this->call(FinancialAidTypesTableSeeder::class);
+         $this->call(IdentityTypesTableSeeder::class);
+         $this->call(TitlesTableSeeder::class);
+         $this->call(PermissionsTableSeeder::class);
          $this->call(RolesTableSeeder::class);
          $this->call(UsersTableSeeder::class);
     }
